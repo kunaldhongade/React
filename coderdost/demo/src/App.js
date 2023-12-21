93c8312e4a1069cfd0910f24b2ed6cb7ed69c70a
kunaldhongade <kunaldhongade1@gmail.com>
2023-12-21 19:12:28 +0530

    fix: clean up

--- coderdost/demo/src/App.js
+++ coderdost/demo/src/App.js
@@ -1,21 +1,87 @@
-import './App.css';
+import "./App.css";
 import Video from "./components/Video";
 
 let DataObj = {
   title: "React Js tutorial",
-  views: '10k',
-  time: '1 years ago',
-  channel: "coder Dost"
-}
+  views: "10k",
+  time: "1 years ago",
+  channel: "coder Dost",
+  verified: true,
+};
+
+let videos = [
+  { ...DataObj },
+  {
+    title: "React Js tutorial",
+    views: "10k",
+    time: "1 years ago",
+    channel: "React Dost",
+    verified: true,
+  },
+  {
+    title: "Node Js tutorial",
+    views: "100k",
+    time: "4 years ago",
+    channel: "Mongo Dost",
+    verified: true,
+  },
+  {
+    title: "MongoDb tutorial",
+    views: "10M",
+    time: "1 years ago",
+    channel: "coder Dost",
+    verified: true,
+  },
+  {
+    title: "internet tutorial",
+    views: "1B",
+    time: "19 years ago",
+    channel: "youtube",
+    verified: true,
+  },
+];
 
 function App() {
+  return (
+    <div className="App">
 
-  return <div className="App">
-    <Video {...DataObj}></Video>
-    <Video title={"Youtube MO"} channel={"Joy Boy"} views={"12M"} time={"1 year ago"} />
-    <Video title={"Github Tutorial"} channel={"King of Pirates"} views={"1B"} time={"1 year ago"} />
-    <Video title={"MongoDB"} channel={"Gear 5"} views={"2M"} time={"1 year ago"} />
-  </div>
+      {
+        videos.map((video) => {
+          return <Video
+            title={video.title}
+            views={video.views}
+            verified={video.verified}
+            channel={video.channel}
+            time={video.time}
+          />
+        })
+      }
+
+
+      {/* <Video {...DataObj}></Video>
+      <Video
+        title={"Youtube MO"}
+        channel={"Joy Boy"}
+        views={"12M"}
+        time={"1 year ago"}
+        verified={true}
+      />
+      <Video
+        title={"Github Tutorial"}
+        channel={"King of Pirates"}
+        views={"1B"}
+        verified={false}
+        time={"1 year ago"}
+      />
+      <Video
+        title={"MongoDB"}
+        channel={"Gear 5"}
+        views={"2M"}
+        verified={true}
+        time={"1 year ago"}
+      /> */}
+    </div>
+  );
 }
 
 export default App;
--- coderdost/demo/src/components/Video.jsx
+++ coderdost/demo/src/components/Video.jsx
@@ -1,14 +1,18 @@
 import './Video.css';
 
-function Video({ title = "Video title", channel = "channel Name", views = "0", time = "0m" }) {
-
+function Video({ title = "Video title", channel = "channel Name", views = "0", time = "0m", verified = false }) {
     return <>
         <div className='container'>
             <div className='pic'>
                 <img src="https://picsum.photos/id/1/160/90" alt="mongo-img" srcset="" />
             </div>
             <div className="title">{title}</div>
-            <div className="channel">{channel}</div>
+            <div className="channel">{channel}
+                {verified ? "✅" : null}
+                {/* we cant write if else in jsx because it does not return anything so things that does not return anything are prohibited in jsx */}
+                {verified && "✅"}
+                {/* there is another thing in react called short circuit && */}
+            </div>
             <div className="views">{views} views <span>.</span>{time}</div>
         </div>
     </>
